13645c33f344af5000a2dabde6f2783bb2b2d4de
imsat <satyajit.cse@yahoo.com>
2024-02-04 02:09:33 +0600

    tailwind setup

---
+++ tailwind-tutorial/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // content: ["./src/**/*.{html,js,vue}"],
+  content: [
+      "./src/**/*.vue",
+      "index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
